efdcc91a8e427125e21f8b04266879e7b18e3017
B1kku <bikku/dev@slmail.me>
2023-07-08 02:32:35 +0200

    Project 04 done, before fill rewrite

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -10,5 +10,37 @@
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
-
-// Put your code here.
+(START)
+@SCREEN
+D = A
+@addr //Addr holds the initial screen's address
+M = D
+(ITERATE)
+@addr
+D = M
+//If addr is <= than KBD
+@KBD
+D = D - A
+@START
+D;JGE
+@KBD //If key pressed, set color to black
+D = M
+@BLACK
+D;JNE
+@color //Else, white
+M = 0
+(COLORDONE)
+@color
+D = M
+@addr //Set address to addr value, make that black.
+A = M
+M = D
+@addr
+M = M + 1
+@ITERATE
+0;JMP
+(BLACK)
+@color
+M = -1
+@COLORDONE
+0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,26 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+//Since R1 is the iterator
+//if R1 == 0; jump to END
+//Else R0 is the value we have to add to the sum.
+//R2 is the sum address.
+//Reduce R1, repeat.
+@R2
+M = 0 //Initialize sum to 0
+(LOOP)
+@R1
+D = M //R1 will work as i, w ffhile i not 0 keep going.
+@END
+D;JEQ //if i == 0, we're done
+@R0
+D = M
+@R2
+M = M + D //Add R0 to sum
+@R1
+M = M - 1 //Reduce i
+@LOOP
+0;JMP
+(END)
+@END
+0;JMP
